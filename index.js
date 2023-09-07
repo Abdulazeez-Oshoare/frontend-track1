f2c05714d451ea5b187ff5e1a74a9656509a2a05
Abdulazez <aaoshoare@gmail.com>
2023-09-07 18:44:32 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const day = document.getElementById("day");
+const d = new Date();
+let daysOfWeek = d.getDay();
+
+switch (daysOfWeek) {
+    case 0: 
+    daysOfWeek = "Sunday";
+        break;
+     case 1: 
+     daysOfWeek = "Monday";
+        break;
+      case 2: 
+    daysOfWeek = "Tuesday";
+        break
+     case 3: 
+    daysOfWeek = "Wednesday";
+        break
+      case 4: 
+    daysOfWeek = "Thursday";
+        break
+      case 5:
+        daysOfWeek =  "Friday";
+        break
+       case 6: 
+       daysOfWeek = "Saturday";
+}
+
+
+day.innerHTML = "Current Day of the Week: " + daysOfWeek;
+
+const redElement = document.getElementById("red"); 
+const currentUTCDate = new Date();
+
+const hours = currentUTCDate.getUTCHours();
+const minutes = currentUTCDate.getUTCMinutes();
+
+const currentUTCTime = `${hours}:${minutes}`;
+
+red.innerHTML = "Current UTC Time: " + currentUTCTime;
+
+
+
+
+
+
