471b75dc7ff84016e04eb228794d3b61d77d773c
Abdulazez <aaoshoare@gmail.com>
2023-09-08 20:06:15 +0100

    revised

--- index.js
+++ index.js
@@ -1,61 +1,27 @@
-const red = document.getElementById("red");
+const redElement = document.getElementById("red");
+const wessElement = document.getElementById("wess");
 
-setInterval(() => {
-  
-  const currentUTCDate = new Date();
-  const hours = currentUTCDate.getUTCHours() + 1;
-  const minutes = currentUTCDate.getUTCMinutes();
-  const seconds = currentUTCDate.getUTCSeconds();
-  const milliseconds = currentUTCDate.getUTCMilliseconds();
+function updateDateTime() {
+    const currentUTCDate = new Date();
+    const hours = currentUTCDate.getUTCHours();
+    const minutes = currentUTCDate.getUTCMinutes();
+    const seconds = currentUTCDate.getUTCSeconds();
+    const milliseconds = currentUTCDate.getUTCMilliseconds();
 
-  const format = hours < 12 ? "AM" : "PM";
+    const format = hours < 12 ? "AM" : "PM";
 
-  
-  if (hours > 12) {
-    hours -= 12;
-  } else if (hours === 0) {
-    hours = 12; 
-  }
-  const formattedHours = hours < 10 ? "0" + hours : hours;
-  const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
-  const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+    const formattedHours = hours % 12 || 12; 
+    const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+    const formattedMilliseconds = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
 
+    redElement.innerHTML = `Current UTC time: ${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds} ${format}`;
 
-  red.innerHTML = "Current UTC time: " + formattedHours + ":" + formattedMinutes + ":" + formattedSeconds + "." + milliseconds +" " + format ;
-}, 1000);
-
-
-
-
-
-
-const wess = document.getElementById("wess");
-const d = new Date();
-let daysOfWeek = d.getDay();
-
-switch (daysOfWeek) {
-    case 0: 
-        daysOfWeek = "Sunday";
-        break;
-    case 1: 
-        daysOfWeek = "Monday";
-        break;
-    case 2: 
-        daysOfWeek = "Tuesday";
-        break;
-    case 3: 
-        daysOfWeek = "Wednesday";
-        break;
-    case 4: 
-        daysOfWeek = "Thursday";
-        break;
-    case 5:
-        daysOfWeek = "Friday";
-        break;
-    case 6: 
-        daysOfWeek = "Saturday";
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const dayOfWeek = daysOfWeek[currentUTCDate.getUTCDay()];
+    wessElement.innerHTML = `Current Day of the Week: ${dayOfWeek}`;
 }
 
-wess.innerHTML = "Current Day of the Week: " + daysOfWeek;
 
-
+updateDateTime();
+setInterval(updateDateTime, 1000);
